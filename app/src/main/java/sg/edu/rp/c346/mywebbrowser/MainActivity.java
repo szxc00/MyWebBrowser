9614a627b1b4331f767b9e9b21c1a0ee8e8468b7
CHIA PEI EN SHARMAIN <16043529@rp.edu.sg>
2018-08-06 11:46:40 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mywebbrowser/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.mywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.google.com";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
